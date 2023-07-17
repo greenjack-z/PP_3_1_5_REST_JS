b3c6e0997cbd9ac63f87e9d56b3a3e2f5ac1dba8
greenjack <e.zelyanin@gmail.com>
2023-07-17 17:43:47 +0300

    add user lastname add user email add user creation date add user password creation date add user locked

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -1,9 +1,11 @@
 package ru.kata.spring.boot_security.demo.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
@@ -17,13 +19,16 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = false, unique = true)
-    private String username;
+    private String email;
 
     @Column(nullable = false)
     private String password;
 
     @Column(nullable = false)
-    private String name;
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
 
     @Column(nullable = false)
     private int age;
@@ -32,7 +37,18 @@ public class User implements UserDetails {
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles;
 
+    @Column(name = "is_locked", nullable = false)
+    private boolean locked;
+    @Column(name = "is_enabled", nullable = false)
     private boolean enabled;
+
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDate createdDate;
+
+    @Column(name = "password_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDate passwordDate;
 
     public User() {
         //empty constructor for Entity
@@ -48,11 +64,11 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return username;
+        return email;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @Override
@@ -62,14 +78,23 @@ public class User implements UserDetails {
 
     public void setPassword(String password) {
         this.password = password;
+        passwordDate = LocalDate.now();
     }
 
-    public String getName() {
-        return name;
+    public String getFirstname() {
+        return firstname;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
     }
 
     public int getAge() {
@@ -93,32 +118,32 @@ public class User implements UserDetails {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return age == user.age && Objects.equals(name, user.name);
+        return age == user.age && Objects.equals(firstname, user.firstname);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, age);
+        return Objects.hash(firstname, age);
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles;
+        return getRoles();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return true;
+        return createdDate.plusYears(1).isAfter(LocalDate.now());
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return true;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return true;
+        return passwordDate.plusMonths(1).isAfter(LocalDate.now());
     }
 
     @Override
@@ -126,7 +151,4 @@ public class User implements UserDetails {
         return enabled;
     }
 
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
 }
